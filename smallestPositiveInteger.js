f2256a044a7b847a05d1da967bd2284b09bc4a2d
AmenNkala <amennkala@gmail.com>
2025-01-31 23:16:01 +0200

    my solution

---
+++ smallestPositiveInteger.js
@@ -0,0 +1,13 @@
+function findSmallestPositiveInt(arr) {
+  const set = new Set(arr);
+  const max = Math.max(...arr);
+
+  if (!set.has(1)) return 1;
+  else {
+    for (let i = 1; i <= max; i++) {
+      if (!set.has(i)) {
+        return i;
+      } else return max + 1;
+    }
+  }
+}
